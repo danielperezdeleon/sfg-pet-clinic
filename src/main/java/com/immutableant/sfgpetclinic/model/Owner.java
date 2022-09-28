d033412613ea2c2de8ccc0537b5f726cc37e4c9b
Daniel Perez de Leon <daniel.perezdeleon@immutableant.com>
2022-09-28 14:13:38 -0700

    adding POJOS. closes #2

---
+++ src/main/java/com/immutableant/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.immutableant.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/immutableant/sfgpetclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package com.immutableant.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+
+}
