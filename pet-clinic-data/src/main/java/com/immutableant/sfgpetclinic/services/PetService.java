07feea725afb9a57389329f8aa30841eb9d0d139
Daniel Perez de Leon <daniel.perezdeleon@immutableant.com>
2022-10-10 12:21:45 -0700

    Interfaces for services created. Closes #5

---
+++ pet-clinic-data/src/main/java/com/immutableant/sfgpetclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package com.immutableant.sfgpetclinic.services;
+
+import com.immutableant.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
---
+++ pet-clinic-data/src/main/java/com/immutableant/sfgpetclinic/services/VetService.java
@@ -0,0 +1,17 @@
+package com.immutableant.sfgpetclinic.services;
+
+import com.immutableant.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findByLastName(String lastName);
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+    
+}
