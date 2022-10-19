992613f78e5f4291c7e93226956cd92d1cc3d632
Daniel Perez de Leon <daniel.perezdeleon@immutableant.com>
2022-10-19 14:25:37 -0700

    Added Specialty Service. closes #33

---
+++ pet-clinic-data/src/main/java/com/immutableant/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,5 @@
+package com.immutableant.sfgpetclinic.services;
+
+import com.immutableant.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {}
