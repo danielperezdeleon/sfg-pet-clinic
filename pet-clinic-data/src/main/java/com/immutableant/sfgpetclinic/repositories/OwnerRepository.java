61e7ad484ccec9f717784e586403da593d59ab53
Daniel Perez de Leon <daniel.perezdeleon@immutableant.com>
2022-10-24 14:18:41 -0700

    Added spring data jpa service for oweners. closes #42

--- pet-clinic-data/src/main/java/com/immutableant/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/immutableant/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,4 +3,6 @@ package com.immutableant.sfgpetclinic.repositories;
 import com.immutableant.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepository extends CrudRepository<Owner, Long> {}
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+  Owner findByLastName(String lastName);
+}
