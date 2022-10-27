9b1665723ff0deb74c36146e3281d8009974cfc0
Daniel Perez de Leon <daniel.perezdeleon@immutableant.com>
2022-10-27 14:16:44 -0700

    Already loaded, but did other stuff. closes #4

--- pet-clinic-web/src/test/java/com/immutableant/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/immutableant/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -3,6 +3,7 @@ package com.immutableant.sfgpetclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
